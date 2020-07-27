b013f19997fc943ffc9d4b7b68ee2a261fe74418
Nick Chen <chenyahua2012@hotmail.com>
2020-07-27 16:59:58 -0400

    print product list on browser

--- products/views.py
+++ products/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from .models import Product
+from .models import Product, Image
 
 # Create your views here.
 def product_list(req):
     products = Product.objects.all().order_by('date')
-    return render(req, 'products/product_list.html', {'products': products})
+    images = Image.objects.all()
+    return render(req, 'products/product_list.html', {'products': products, 'images': images})
