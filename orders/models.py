f4b325a63c254239c342b237e6ed5c738a1ff14b
Nick Chen <chenyahua2012@hotmail.com>
2020-07-29 00:08:42 -0400

    add orders models

--- orders/models.py
+++ orders/models.py
@@ -1,7 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import User
+from products.models import Product
 
 # Create your models here.
-# class Order(models.Model):
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
+    date = models.DateTimeField(auto_now_add=True)   
 
-#     customer = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
+    def __str__(self):
+        return '%s ordered on -> %s' % (self.user,self.date)
+
+class OrderProduct(models.Model):      
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, default=None)
+    product = models.CharField(max_length=100)
+    size = models.CharField(max_length=16)
+    quantity = models.IntegerField()
+    subtotal = models.IntegerField()
+
+    def __str__(self):
+        return 'Order number: %s Subtotal: $%s' % (self.order.id,self.subtotal)
