e959cb0d2887102105d59991732ae18e6fc8720f
Nick Chen <chenyahua2012@hotmail.com>
2020-08-03 16:31:27 -0400

    make sure to let user login before the payment

--- assets/header.js
+++ assets/header.js
@@ -1,31 +1,31 @@
 const drop_menu = document.getElementById('drop_menu');
 const hover_menu = document.getElementById('hover_menu');
 
-if(drop_menu){
-    drop_menu.addEventListener('mouseover', ()=>{
-        hover_menu.style.display='grid';
-    })
-    hover_menu.addEventListener('mouseover', ()=>{
-        hover_menu.style.display='grid';
-    })
-    drop_menu.addEventListener('mouseleave', ()=>{
-        hover_menu.style.display='none';
-    })
-    hover_menu.addEventListener('mouseleave', ()=>{
-        hover_menu.style.display='none';
-    })
-}
-
-let itemNum = 0;
-let cookiesArr = document.cookie.split('; ');
-for(let i=0;i<cookiesArr.length;i++){
-    if(cookiesArr[i].split('=')[0].includes('Lets_shop')){
-        itemNum+=Number(JSON.parse(cookiesArr[i].split('=')[1]).quantity)
+    if(hover_menu){
+        drop_menu.addEventListener('mouseover', ()=>{
+            hover_menu.style.display='grid';
+        })
+        drop_menu.addEventListener('mouseleave', ()=>{
+            hover_menu.style.display='none';
+        })
+        hover_menu.addEventListener('mouseover', ()=>{
+            hover_menu.style.display='grid';
+        })
+        hover_menu.addEventListener('mouseleave', ()=>{
+            hover_menu.style.display='none';
+        })
     }
-}
-if(itemNum>0){
-    document.getElementById('item_num').style.display = 'flex';
-    document.getElementById('item_num').innerHTML = itemNum
-}else{
-    document.getElementById('item_num').style.display = 'none';
-}
+
+    let itemNum = 0;
+    let cookiesArr = document.cookie.split('; ');
+    for(let i=0;i<cookiesArr.length;i++){
+        if(cookiesArr[i].split('=')[0].includes('Lets_shop')){
+            itemNum+=Number(JSON.parse(cookiesArr[i].split('=')[1]).quantity)
+        }
+    }
+    if(itemNum>0){
+        document.getElementById('item_num').style.display = 'flex';
+        document.getElementById('item_num').innerHTML = itemNum;
+    }else{
+        document.getElementById('item_num').style.display = 'none';
+    }
