ea1e49008da5e9d6585c535cf714886c7bd252b3
Nick Chen <chenyahua2012@hotmail.com>
2020-08-03 16:35:55 -0400

    redirect to login page, if the user is anonymous

--- src/components/Order_list.vue
+++ src/components/Order_list.vue
@@ -7,15 +7,17 @@
             <span>Size</span>
             <span>Subtotal</span>
         </div>
-        <div class='item_info' v-for='item in arr' :key='item.product_id'>
-            <div class='product_img'>
-                <img :src="item.primary_image" width='100'>
+            <div class='item_info' v-for='item in arr' :key='item.product_id'>
+                <div class='product_img'>
+                    <img :src="item.primary_image" width='100'>
+                </div>
+                <div>{{ item.title }}</div>
+                <input type='number' v-on:click='updateCookie(item.product_id)' v-model='item.quantity' class='item_quantity' min='1'>
+                <div>{{ item.size }}</div>
+                <div>${{ item.price * item.quantity }}</div>
             </div>
-            <div>{{ item.title }}</div>
-            <input type='number' v-on:click='updateCookie(item.product_id)' v-model='newQuantity' class='item_quantity' min='1' placeholder='1'>
-            <div>{{ item.size }}</div>
-            <div>${{ item.price * item.quantity }}</div>
-        </div>
+            <button v-if='customer.length>0'>Process to Payment</button>
+            <button v-else v-on:click='redirect()'>Login First</button>
     </div>
 </template>
 
@@ -23,10 +25,11 @@
 <script>
     export default {
         name: "OrderView",
+        delimiters: ['[[', ']]'],
         data(){
             return {
                 arr: [],
-                newQuantity: '1'
+                customer: document.getElementById('drop_menu').getAttribute('data')
             }
         },
         methods: {
@@ -41,10 +44,28 @@
             updateCookie(id){
                 this.arr.forEach(e=>{
                     if(e.product_id==id){
-                        e.quantity = this.newQuantity;
                         document.cookie = 'Lets_shop_'+e.product_id+'='+JSON.stringify(e)+';path=/';
+                        this.itemCounter()
                     }
                 })
+            },
+            itemCounter(){
+                let itemNum = 0;
+                let cookiesArr = document.cookie.split('; ');
+                for(let i=0;i<cookiesArr.length;i++){
+                    if(cookiesArr[i].split('=')[0].includes('Lets_shop')){
+                        itemNum+=Number(JSON.parse(cookiesArr[i].split('=')[1]).quantity)
+                    }
+                }
+                if(itemNum>0){
+                    document.getElementById('item_num').style.display = 'flex';
+                    document.getElementById('item_num').innerHTML = itemNum
+                }else{
+                    document.getElementById('item_num').style.display = 'none';
+                }
+            },
+            redirect(){
+                window.location.href="http://127.0.0.1:8000/accounts/login/"
             }
         },
         beforeMount(){
